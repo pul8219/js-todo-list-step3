001bf4cdf604c67f800f3834b6ac2bf7716ac483
Junho U <50912829+kode15333@users.noreply.github.com>
2020-12-22 22:39:41 +0900

    Revert "[5기 Junho U] TodoList for Team"

--- js/util/router.js
+++
@@ -1,7 +0,0 @@
-export const isKanban = () => {
-  return location.pathname.search('kanban') > 0;
-}
-
-export const toKanban = (teamInfo) => {
-  return history.pushState(teamInfo, '', 'kanban.html');
-}
